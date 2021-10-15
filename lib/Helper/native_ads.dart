054121b5f260493828ac4fb82dd35322cfbfd04b
Anton Wyrowski <anton.wyrowski@gmail.com>
2021-10-15 22:26:34 +0200

    Delete lib/Helper directory

--- lib/Helper/native_ads.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/services.dart';
-
-class NativeAds {
-  NativeAds.initialize() {
-    _channel.invokeMethod<void>('initialize');
-  }
-  static const MethodChannel _channel = MethodChannel('native_ads');
-}
--- lib/Helper/shareable.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:one_d_m/helper/share_image.dart';
-
-mixin Shareable {
-  Future<String> getShareUrl(BuildContext? context);
-  Future<InstagramImages?> getShareImages(BuildContext? context);
-}
